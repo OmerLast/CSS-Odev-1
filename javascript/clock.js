6c28585628cd3e021677bbd423f2f3065731bb34
OmerLast <omercanmankaloglu@gmail.com>
2022-04-28 15:06:14 +0300

    tarih eklendi

--- javascript/clock.js
+++ javascript/clock.js
@@ -6,6 +6,29 @@ function showTime(){
     var h = date.getHours(); // 0 - 23
     var m = date.getMinutes(); // 0 - 59
     var s = date.getSeconds(); // 0 - 59
+    var date = date.getDay();
+    
+    if (date == "1") {
+        date = "Pazartesi";
+    }
+    if (date == "2") {
+        date = "Sali";
+    }
+    if (date == "3") {
+        date = "Carsamba";
+    }
+    if (date == "4") {
+        date = "Persembe";
+    }
+    if (date == "5") {
+        date = "Cuma";
+    }
+    if (date == "6") {
+        date = "Cumartesi";
+    }
+    if (date == "7") {
+        date = "Pazar";
+    }
     var session = "AM";
     if(h == 0){
         h = 12;
@@ -18,8 +41,8 @@ function showTime(){
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;  
     var time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("myClock").innerText = time;
-    document.getElementById("myClock").textContent = time;
+    document.getElementById("myClock").innerText = time + " " + date;
+    document.getElementById("myClock").textContent = time + " " + date;
     setTimeout(showTime, 1000);
 }
 showTime();
